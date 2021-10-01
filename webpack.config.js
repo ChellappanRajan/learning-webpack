3dc5fc57e3e29a2ec78fcea6f18156606e4c92e0
Chellappan <chellappan@Chellappans-MacBook-Pro.local>
2021-10-01 22:55:01 +0530

    Added built utils

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,20 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require("webpack");
+const { merge } = require('webpack-merge');
+
+const modeConfig = (env='development')=>require(`./build-utils/webpack.${env}`)(env);
+
 module.exports =({mode})=>{
-    return {
-        mode,
-        entry:"./src/index.js",
-        output:{
-            filename:"bundle.js"
+    console.log(mode,`./build-utils/webpack.${mode}`);
+    return merge(
+        {
+            mode,
+            entry:"./src/index.js",
+            output:{
+                filename:"bundle.js"
+            },
+            plugins:[new HtmlWebpackPlugin(), new webpack.ProgressPlugin()]
         },
-        plugins:[new HtmlWebpackPlugin(), new webpack.ProgressPlugin()]
-    }
+        modeConfig(mode)
+    )
 }
